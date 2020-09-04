34e66cf6f3ba5e1d36cd990b80dc41193c72ffa5
AlexIslander <sandor@csetesandor.hu>
2020-09-04 13:43:17 +0200

    update - code fixes
    - angular.json adjustments
    - prod build corrections

--- src/app/shared/autocomplete/base-autocomplete.component.ts
+++ src/app/shared/autocomplete/base-autocomplete.component.ts
@@ -1,6 +1,6 @@
 import {AppConstants} from '../helpers/AppConstants';
 import {FormControl} from '@angular/forms';
-import { Input, ViewChild, Directive } from '@angular/core';
+import {Input, ViewChild, Directive, Component} from '@angular/core';
 import { MatAutocompleteTrigger } from '@angular/material/autocomplete';
 import {Named} from '../dto/Named';
 import {debounceTime, distinctUntilChanged, map, startWith} from 'rxjs/operators';
@@ -10,8 +10,10 @@ import {CommonFunctionService, has} from '../service/common-function.service';
 import {combineLatest} from 'rxjs/internal/observable/combineLatest';
 import {of} from 'rxjs/internal/observable/of';
 
-@Directive()
-export abstract class BaseAutocompleteDirective extends BaseCustomComponent {
+@Component({
+  template: ''
+})
+export abstract class BaseAutocompleteComponent extends BaseCustomComponent {
   @ViewChild(MatAutocompleteTrigger, {static: true}) autoComplete: MatAutocompleteTrigger;
 
   @Input() allowInputDelete: boolean;
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -33,6 +33,7 @@ import {CustomDatePickerHeaderComponent} from './component/date-picker/custom-da
 import {FormErrorMessageModule} from './form-error-message/form-error-message.module';
 import { CheckboxGroupComponent } from './component/checkbox-group/checkbox-group.component';
 import { RadioGroupComponent } from './component/radio-group/radio-group.component';
+import {DynamicField} from './dynamic-fields/dynamic-field';
 
 @NgModule({
   imports: [
@@ -71,7 +72,8 @@ import { RadioGroupComponent } from './component/radio-group/radio-group.compone
     DatePickerComponent,
     CustomDatePickerHeaderComponent,
     CheckboxGroupComponent,
-    RadioGroupComponent
+    RadioGroupComponent,
+    DynamicField
   ],
   exports: [
     SiteHeaderComponent,
